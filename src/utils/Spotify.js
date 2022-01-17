422ea4fd56f2696b5693c6f6a375e6474e33f0d6
glitcher93 <nigeljdsouza@gmail.com>
2022-01-16 19:34:38 -0500

    testing a possible bug fix

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -19,7 +19,7 @@ const Spotify = {
             accessToken = accessTokenMatch[1];
             const expiresIn = Number(expiresInMatch[1]);
             window.setTimeout(() => accessToken = '', expiresIn * 1000);
-            window.history.pushState('Access Token', null, '/');
+            // window.history.pushState('Access Token', null, '/');
             return accessToken;
         } else {
             const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`
